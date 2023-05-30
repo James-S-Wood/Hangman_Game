ea7398c89b0e6aa68b728103f009e0a5d18e54a8
James Wood <james.s.wood@icloud.com>
2023-05-30 04:54:21 -0400

    Refactored Code.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import random
+import tkinter as tk
+from tkinter import messagebox
 
+"""This code uses the Tkinter library to create a simple GUI for the Hangman Game. The main game logic remains
+ the same, but now the game interface is displayed in a window with interactive elements. The code creates a 
+ window using tkinter.Tk(), adds labels to display the game title, the guessed word, and the number of tries 
+ left. It also includes an entry field for the user to input their guesses and a button to check their guess.
+"""
 
 def hangman():
     words = ["python", "programming", "computer", "hangman", "openai"]
@@ -9,82 +16,17 @@ def hangman():
     guessed_letters = []
     guessed_word = ["_"] * word_length
 
-    """In this updated version, I added a list called gallows that contains ASCII art representations 
-    of the hangman's gallows at different stages. Each stage corresponds to the number of tries left in 
-    the game. The gallows art is displayed before each guess, providing a visual representation of the 
-    hangman's progress."""
-
-    gallows = [
-        '''
-           +---+
-               |
-               |
-               |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-               |
-               |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-           |   |
-               |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-          /|   |
-               |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-          /|\\  |
-               |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-          /|\\  |
-          /    |
-              ===
-        ''',
-        '''
-           +---+
-           O   |
-          /|\\  |
-          / \\  |
-              ===
-        '''
-    ]
-
-    print("Welcome to Hangman!")
-    print("Guess the word by entering one letter at a time.")
-    print("You have 6 tries to guess the word correctly.")
-
-    while tries > 0:
-        print("\n")
-        print(gallows[6 - tries])
-        print(" ".join(guessed_word))
-        print("Tries left:", tries)
-
-        guess = input("Enter your guess: ").lower()
+    def check_guess():
+        guess = guess_entry.get().lower()
+        guess_entry.delete(0, tk.END)
 
         if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single letter.")
-            continue
+            messagebox.showwarning("Invalid Guess", "Please enter a single letter.")
+            return
 
         if guess in guessed_letters:
-            print("You already guessed that letter. Try again.")
-            continue
+            messagebox.showinfo("Duplicate Guess", "You already guessed that letter. Try again.")
+            return
 
         guessed_letters.append(guess)
 
@@ -94,24 +36,67 @@ def hangman():
                     guessed_word[i] = guess
 
             if "_" not in guessed_word:
-                print("\nCongratulations! You guessed the word correctly:", selected_word)
-                return
+                messagebox.showinfo("Congratulations", f"You guessed the word correctly: {selected_word}")
+                window.destroy()
         else:
+            nonlocal tries
             tries -= 1
-            print("Wrong guess. Try again.")
 
-    print("\nGame over! You ran out of tries.")
-    print("The word was:", selected_word)
+            if tries == 0:
+                messagebox.showinfo("Game Over", f"You ran out of tries. The word was: {selected_word}")
+                window.destroy()
+            else:
+                # Update the gallows image based on the number of tries left
+                gallows_canvas.itemconfig(gallows_images[6 - tries], state="normal")
+
+        update_game()
+
+    def update_game():
+        tries_label.config(text=f"Tries left: {tries}")
+        guessed_word_label.config(text=" ".join(guessed_word))
+
+    window = tk.Tk()
+    window.title("Hangman")
+    window.resizable(False, False)
+
+    title_label = tk.Label(window, text="Hangman", font=("Arial", 24))
+    title_label.pack(pady=10)
+
+    gallows_canvas = tk.Canvas(window, width=200, height=200)
+    gallows_canvas.pack()
+
+    gallows_images = [
+        gallows_canvas.create_line(20, 180, 180, 180, width=3, state="hidden"),
+        gallows_canvas.create_line(50, 180, 50, 20, width=3, state="hidden"),
+        gallows_canvas.create_line(50, 20, 130, 20, width=3, state="hidden"),
+        gallows_canvas.create_line(130, 20, 130, 50, width=3, state="hidden"),
+        gallows_canvas.create_oval(115, 50, 145, 80, width=3, state="hidden"),
+        gallows_canvas.create_line(130, 80, 130, 130, width=3, state="hidden"),
+        gallows_canvas.create_line(130, 100, 115, 120, width=3, state="hidden"),
+        gallows_canvas.create_line(130, 100, 145, 120, width=3, state="hidden")
+    ]
+
+    word_label = tk.Label(window, text="Word:")
+    word_label.pack()
+
+    guessed_word_label = tk.Label(window, text=" ".join(guessed_word), font=("Arial", 16))
+    guessed_word_label.pack()
+
+    guess_label = tk.Label(window, text="Enter your guess:")
+    guess_label.pack()
+
+    guess_entry = tk.Entry(window, font=("Arial", 16))
+    guess_entry.pack()
+
+    check_button = tk.Button(window, text="Check", command=check_guess)
+    check_button.pack(pady=10)
+
+    tries_label = tk.Label(window, text=f"Tries left: {tries}")
+    tries_label.pack()
+
+    update_game()
+
+    window.mainloop()
 
 
 hangman()
-
-"""In this code, we define the hangman function that represents the main logic of the game. 
-It initializes a list of words, selects a random word from the list, and sets up variables like the word length,
- number of tries, and lists to store guessed letters and the current state of the guessed word. The function 
- then enters a loop where it prompts the user for a guess, checks the validity of the guess, and updates the 
- game state accordingly. If the guessed letter is correct, it updates the guessed word with the correctly 
- guessed letter. If the guessed letter is incorrect, it reduces the number of tries. The game continues until 
- the player either guesses the word correctly or runs out of tries. At the end of the game, an appropriate 
- message is displayed. To play the Hangman game, simply call the hangman function. Have fun coding and enjoy 
- playing Hangman!"""
